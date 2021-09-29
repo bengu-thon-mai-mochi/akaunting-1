fc67baa855ede48f3c520cc49616cb26154c6f95
benguozakinci@gmail.com <akaunting-002@Akaunting-002s-MacBook-Pro.local>
2021-09-29 11:24:48 +0300

    Save work

--- resources/assets/js/views/apps/components/SearchBar.vue
+++ resources/assets/js/views/apps/components/SearchBar.vue
@@ -27,32 +27,51 @@
             </div>
           </div>
           <div class="vr d-none d-sm-block"></div>
-          <div class="col-xs-12 col-sm-6">
+          <div class="col-xs-12 col-sm-7">
             <div class="searh-field tags-input__wrapper">
                <el-autocomplete
                     name="keyword"
+                    class="col"
                     :trigger-on-focus="false"
                     select-when-unmatched	
                     :value="query"
                     @select.native="handleEnter"
-                    @keydown.enter="handleEnter"
+                    @keydown.enter.native="handleEnter"
                     :fetch-suggestions="fetchResults"
                     v-model="query"
-                    popper-class="dropdown-menu dropdown-menu-xl dropdown-menu-center show"
+                    popper-class="list-group-item  list-group-item-active dropdown-menu-xl justify-content-start dropdown-menu-center show"
                     :placeholder="translations.searchBar.search_placeholder"
                     >
                      <template slot-scope="{ item }">
-                        <div class="item">
-                          <img width="30px"/>
-                          <router-link :to="item.slug" >{{ item.value }}</router-link>
-                          <a class="btn btn-warning">Add to Cart</a>
-                          <a class="btn btn-success" :to="item.action_url">Buy Now</a>
+                        <div class="item d-flex justify-content-between align-items-center">
+                            <router-link :to="item.slug" class="item dropdown-item  d-flex justify-content-start list-group-item-active">
+                              <img class="col-sm-2 p-1" max-width="30px"/>
+                              <h5 class="col-sm-6">{{ item.value }}</h5>
+                              <h4 v-show="item.price" class="col-sm-5 font-weight-600">{{ item.price }}</h4>
+                            </router-link>
+                           <div>
+                            <div>
+                              <div v-if="!item.installedApps.includes(item.slug)">
+                                <div v-if="item.price">
+                                  <a class="btn btn-sm badge btn-success text-white" :href="item.action_url">Buy Now</a>
+                                  <button @click="addCart" class="btn btn-sm badge btn-warning text-white">Add to Cart</button> 
+                                </div>
+                                <div v-else>
+                                  <span class="badge btn-sm bg-green text-white">{{ translations.general.free }}</span>
+                                </div>
+                              </div>
+                            
+                              <div v-else> 
+                                <span class="badge bg-green text-white" :to="item.action_url">Installed</span>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </template>
                 </el-autocomplete>
               </div>
             </div>
-                <div class="col-xs-12 col-sm-4 text-center">
+                <div class="col-xs-12 col-sm-3 text-center">
                   <router-link to="/apps/paid" class="btn btn-white btn-sm" exact>{{ translations.general.top_paid }}</router-link>
                   <router-link to="/apps/new" class="btn btn-white btn-sm">{{ translations.general.new }}</router-link>
                   <router-link to="/apps/free" class="btn btn-white btn-sm">{{ translations.general.top_free }}</router-link>
@@ -120,10 +139,12 @@ export default {
         const endPoint = url + '/apps/search?keyword='+ this.query;
 
         const result = await window.axios.get(endPoint).then(res => res);
+
+        const installedApps = Object.keys(result.data.data.installed);
       
         result.data.data.modules ? 
           result.data.data.modules.data.forEach( res =>
-             this.results.push({ "value": res.name , "slug": res.slug, "logo": res.files, "link": res.action_url })
+             this.results.push({ "value": res.name , "slug": res.slug, "logo": res.files, "price": res.price, "link": res.action_url, "installedApps": installedApps, })
           )
           : this.results = [];
       
@@ -135,3 +156,8 @@ export default {
 }
 </script>
 
+<style>
+.dropdown-item {
+  flex-grow: 3;
+}
+</style>
